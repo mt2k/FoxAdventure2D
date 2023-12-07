30a2480f91b0ec022f158c7509b2979a921bd632
mt2k <105912009+mt2k@users.noreply.github.com>
2023-12-07 09:56:59 +0700

    Test Github after change location

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -15,7 +15,7 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Test Message");
     }
 
     // Update is called once per frame
