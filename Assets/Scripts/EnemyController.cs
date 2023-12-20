adbb1fa5eb4f7cf5ed33c4980013ff38321a20cc
mt2k <105912009+mt2k@users.noreply.github.com>
2023-12-21 01:10:08 +0700

    Update Func Moving Frog and FlipX

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,15 +9,39 @@ public class EnemyController : MonoBehaviour
     private bool movingRight;
 
     private Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        leftPoint.parent = null; 
+        rightPoint.parent = null;
+        movingRight = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (movingRight)
+        {
+            rigidbody2D.velocity = new Vector2(moveSpeed, rigidbody2D.velocity.y);
+            spriteRenderer.flipX = true;
+
+            if (transform.position.x > rightPoint.position.x)
+            {
+                movingRight = false;
+            }
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector2(-moveSpeed, rigidbody2D.velocity.y);
+            spriteRenderer.flipX = false;
+
+            if (transform.position.x < leftPoint.position.x)
+            {
+                movingRight = true;
+            }
+        }
     }
 }
