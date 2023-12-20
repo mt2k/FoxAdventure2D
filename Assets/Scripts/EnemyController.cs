1b54f220bbda99aea51453cf93fc955c5b84693e
mt2k <105912009+mt2k@users.noreply.github.com>
2023-12-20 09:55:35 +0700

    Setup Frog Enemy Animation

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform leftPoint, rightPoint;
+    private bool movingRight;
+
+    private Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
