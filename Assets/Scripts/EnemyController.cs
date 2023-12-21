071645ef4c219423380fae8977dddb99d80ddab7
mt2k <105912009+mt2k@users.noreply.github.com>
2023-12-21 08:52:15 +0700

    Adding Pause for Enemy about move and wait

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,37 +10,65 @@ public class EnemyController : MonoBehaviour
 
     private Rigidbody2D rigidbody2D;
     private SpriteRenderer spriteRenderer;
+
+    public float moveTime, waitTime;
+    private float moveCount, waitCount;
+
+    private float MIN_VALUE_WAIT = .75f;
+    private float MAX_VALUE_WAIT = .45f;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+
         leftPoint.parent = null; 
         rightPoint.parent = null;
+
         movingRight = true;
+
+        moveCount = moveTime;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (movingRight)
+        if (moveCount > 0)
         {
-            rigidbody2D.velocity = new Vector2(moveSpeed, rigidbody2D.velocity.y);
-            spriteRenderer.flipX = true;
-
-            if (transform.position.x > rightPoint.position.x)
+            moveCount -= Time.deltaTime;
+            if (movingRight)
             {
-                movingRight = false;
+                rigidbody2D.velocity = new Vector2(moveSpeed, rigidbody2D.velocity.y);
+                spriteRenderer.flipX = true;
+
+                if (transform.position.x > rightPoint.position.x)
+                {
+                    movingRight = false;
+                }
             }
-        }
-        else
-        {
-            rigidbody2D.velocity = new Vector2(-moveSpeed, rigidbody2D.velocity.y);
-            spriteRenderer.flipX = false;
-
-            if (transform.position.x < leftPoint.position.x)
+            else
             {
-                movingRight = true;
+                rigidbody2D.velocity = new Vector2(-moveSpeed, rigidbody2D.velocity.y);
+                spriteRenderer.flipX = false;
+
+                if (transform.position.x < leftPoint.position.x)
+                {
+                    movingRight = true;
+                }
+            } 
+            
+            if(moveCount <= 0) {
+                waitCount = Random.Range(waitTime * MIN_VALUE_WAIT, waitTime * MAX_VALUE_WAIT);
+            }
+        } else if(waitCount > 0) 
+        {
+            waitCount -= Time.deltaTime;
+            rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
+            if(waitCount <= 0)
+            {
+                moveCount = Random.Range(moveTime * MIN_VALUE_WAIT, moveTime * MAX_VALUE_WAIT);
             }
         }
     }
